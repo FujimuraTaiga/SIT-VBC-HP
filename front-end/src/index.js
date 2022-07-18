fa62a025e826792ec35ea94951b3198e4ca9be72
taiga-mac <fujimurataiga@icloud.com>
2022-07-18 23:25:48 +0900

    トップページ作成

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import TopPage from "./pages/top_page";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    
+    <TopPage/>
   </React.StrictMode>
 );
---
+++ front-end/src/pages/top_page.js
@@ -0,0 +1,5 @@
+const TopPage = () => {
+    return <h1>Top Page</h1>
+}
+
+export default TopPage;
